6c2cf7f999f421edd2cb37259a41c0cb3f3e356b
Lance Lopez <lancelopez46@gmail.com>
2023-02-22 16:33:16 +0800

    update time with date-fns

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,11 +1,22 @@
+const units = "metric";
+
+let temperatureUnit;
+let speedUnit;
+
+if (units === "metric") {
+  temperatureUnit = "°C";
+  speedUnit = "kp/h";
+} else {
+  temperatureUnit = "°F";
+  speedUnit = "mph";
+}
+
 // top, left side - current forcast
 const descriptionValue = document.querySelector("h1");
-const cityValue = document.querySelector(
+const location = document.querySelector(
   ".current-weather-left>span:nth-child(2)"
 );
-const countryValue = document.querySelector(
-  ".current-weather-left>span:nth-child(3)"
-);
+
 const tempValue = document.querySelector(
   ".current-weather-left>span:nth-child(4)"
 );
@@ -19,20 +30,19 @@ function displayCurrentWeatherLeft(
   temperature
 ) {
   descriptionValue.textContent = description;
-  weatherIcon.src = `http://openweathermap.org/img/w/${icon}.png`;
-  cityValue.textContent = city;
-  countryValue.textContent = country;
-  tempValue.textContent = temperature;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  location.textContent = `${city}, ${country}`;
+  tempValue.textContent = `${temperature} ${temperatureUnit}`;
 }
 
 // top, right side - current forcast
 const feelsLikeValue = document.querySelector(
   ".right-box:nth-child(1) span:nth-child(3)"
 );
-const minTempValue = document.querySelector(
+const maxTempValue = document.querySelector(
   ".right-box:nth-child(2) span:nth-child(3)"
 );
-const maxTempValue = document.querySelector(
+const minTempValue = document.querySelector(
   ".right-box:nth-child(3) span:nth-child(3)"
 );
 const humidityValue = document.querySelector(
@@ -49,29 +59,41 @@ function displayCurrentWeatherRight(
   humidity,
   windSpeed
 ) {
-  feelsLikeValue.textContent = feelsLike;
-  minTempValue.textContent = tempMin;
-  maxTempValue.textContent = tempMax;
-  humidityValue.textContent = humidity;
-  windSpeedValue.textContent = windSpeed;
+  feelsLikeValue.textContent = `${feelsLike} ${temperatureUnit}`;
+  minTempValue.textContent = `${tempMin} ${temperatureUnit}`;
+  maxTempValue.textContent = `${tempMax} ${temperatureUnit}`;
+  humidityValue.textContent = `${humidity}%`;
+  windSpeedValue.textContent = `${windSpeed} ${speedUnit}`;
 }
 
 // bottom - 3h 5d forcast
 const forecastContainer = document.querySelector(".forecast-container");
 
-function createForcast(temperature, icon, dateTime) {
+function createForcast(temperature, time, icon, day, date, status) {
   const newForcast = document.createElement("div");
   newForcast.classList.add("forecast");
-
+  if (status === "hide") {
+    newForcast.classList.add("hide");
+  }
   const forecastTemperature = document.createElement("span");
   const forecastIcon = document.createElement("img");
-  const forecastDateTime = document.createElement("span");
+  const forecastTime = document.createElement("span");
+  const forecastDay = document.createElement("span");
+  const forecastDate = document.createElement("span");
 
-  forecastTemperature.textContent = temperature;
-  forecastIcon.src = `http://openweathermap.org/img/w/${icon}.png`;
-  forecastDateTime.textContent = dateTime;
+  forecastTemperature.textContent = `${temperature} ${temperatureUnit}`;
+  forecastIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  forecastTime.textContent = time;
+  forecastDay.textContent = day;
+  forecastDate.textContent = date;
 
-  newForcast.append(forecastTemperature, forecastIcon, forecastDateTime);
+  newForcast.append(
+    forecastTemperature,
+    forecastTime,
+    forecastIcon,
+    forecastDay,
+    forecastDate
+  );
 
   forecastContainer.append(newForcast);
 }
--- src/modules/weatherForcast.js
+++ src/modules/weatherForcast.js
@@ -1,16 +1,31 @@
 import { createForcast } from "./dom";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
 
 function updateWeatherForcast(weatherData) {
   console.log(weatherData);
+
   for (let i = 0; i < weatherData.list.length; i++) {
-    console.log(weatherData.list[i].main.temp);
-    console.log(weatherData.list[i].weather[0].icon);
-    console.log(weatherData.list[i].dt_txt);
-    createForcast(
-      weatherData.list[i].main.temp,
-      weatherData.list[i].weather[0].icon,
-      weatherData.list[i].dt_txt
-    );
+    let convertedTime = parseISO(weatherData.list[i].dt_txt);
+
+    if (i < 8) {
+      createForcast(
+        weatherData.list[i].main.temp,
+        format(convertedTime, "p"),
+        weatherData.list[i].weather[0].icon,
+        format(convertedTime, "iiii"),
+        format(convertedTime, "MMM, dd")
+      );
+    } else {
+      createForcast(
+        weatherData.list[i].main.temp,
+        format(convertedTime, "p"),
+        weatherData.list[i].weather[0].icon,
+        format(convertedTime, "iiii"),
+        format(convertedTime, "MMM, dd"),
+        "hide"
+      );
+    }
   }
 }
 
