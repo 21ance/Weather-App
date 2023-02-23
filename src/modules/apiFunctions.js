98a244b87dea6a48df2ff059d20bf0647e3a0e1c
Lance Lopez <lancelopez46@gmail.com>
2023-02-23 19:08:19 +0800

    geocoding http to https

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -2,7 +2,7 @@ import { renderWeather, renderGeocoding } from "./renderer";
 
 const FORECAST_WEATHER = "https://api.openweathermap.org/data/2.5/forecast?q=";
 const CURRENT_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=";
-const GEOCODING = "http://api.openweathermap.org/geo/1.0/direct?q=";
+const GEOCODING = "https://api.openweathermap.org/geo/1.0/direct?q=";
 const API_KEY = "&APPID=28c57ffd486c8684f804fc9a6681f59a&units=";
 
 async function fetchWeather(location, units) {
